fcd24a260fdc73929809fd7065cbe49d50027c60
abhijeetjain99 <abhijeetjain99@gmail.com>
2018-09-25 00:52:13 +0530

    Added - BitwiseArgumentsInMethodTests1. - FluentAssertions referance. - stylecop fix.

---
+++ Tests/Tests/Standards/Attributes.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests.Standards
+{
+    using System;
+
+    /// <summary>
+    /// [Flags] attribute allows to use HasFlas method on enum 
+    /// which behind the scene operate like a Bitwise AND (both true then true)
+    /// Example- 
+    ///         10110
+    ///         01101 &
+    ///        -------
+    ///         00100
+    /// </summary>
+    [Flags]
+    public enum Attributes : long
+    {
+        /// <summary>
+        /// Represents Bitwise 1 = 0001 shifted 0 times.
+        /// </summary>
+        Setting1 = 1L << 0,
+
+        /// <summary>
+        /// Represents Bitwise 1 = 0010 shifted 1 times.
+        /// </summary>
+        Setting2 = 1L << 1,
+
+        /// <summary>
+        /// Represents Bitwise 1 = 0100 shifted 2 times.
+        /// </summary>
+        Setting3 = 1L << 2,
+
+        /// <summary>
+        /// Represents Bitwise 1 = 1000 shifted 3 times.
+        /// </summary>
+        Setting4 = 1L << 3
+    }
+}
---
+++ Tests/Tests/Standards/BitwiseArgumentsInMethodTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests.Standards
+{
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Summary description for BitwiseArgumentsInMethodTests
+    /// </summary>
+    [TestClass]
+    public class BitwiseArgumentsInMethodTests
+    {
+        /// <summary>
+        /// chech the functionality of the Bitwise operator  when Enum is attributed as [Flags].
+        /// </summary>
+        [TestMethod]
+        public void BitwiseArgumentsInMethodTests1()
+        {
+            Attributes attributeObject = Attributes.Setting1 | Attributes.Setting3;
+
+            attributeObject.HasFlag(Attributes.Setting2).Should().BeFalse("attributeObject do not have flag fot setting2");
+            attributeObject.HasFlag(Attributes.Setting1).Should().BeTrue("attributeObject has flag for setting1");
+        }
+    }
+}
